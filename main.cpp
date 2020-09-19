6b8bbe06c70fbec30cdf0de25fb8dc7cc272a304
Nico Jahn <info@nicojahn.com>
2020-09-19 13:11:35 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -5,20 +5,19 @@
 #define bug 1
 
 using namespace HighFive;
-
 int main(int argc, char** argv) {
-    if (argc != 2) return 0;
+    if (argc != 2) return 1;
     File file(argv[1], File::ReadOnly);
 
     Group g = file.getGroup("/");
-    for (auto elem : g.listAttributeNames()) {
-        std::cout << elem << std::endl;
+    for (auto attribute_name : g.listAttributeNames()) {
+        std::cout << attribute_name << std::endl;
 
-        auto attribute = g.getAttribute(elem);
+        auto attribute = g.getAttribute(attribute_name);
         std::string read;
 
 #if bug
-        if (elem != "model_config") continue;
+        if (attribute_name != "model_config") continue;
 #endif
 
         attribute.read(read);
